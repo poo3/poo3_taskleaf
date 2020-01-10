9e2950196bad0ed00a040a7d0000e992235a301a
バントウフウマ <coolishpoo3@gmail.com>
2020-01-10 11:00:42 +0900

    作成したタスクをCSVで出力する機能追加しました

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,17 @@
 class Task < ApplicationRecord
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|
+      csv << csv_attributes
+      all.each do |task|
+        csv << csv_attributes.map{|attr| task.send(attr)}
+      end
+    end
+  end
+
   has_one_attached :image
   # validates :name, presence: true
   validates :name, length: {maximum:30}
